8aea0a17e9b1a73986bc4c7185fb4c370c407af9
JorgeMNovacia <jorge.mendes@novacia.com.br>
2017-07-07 21:41:49 -0300

    ajustes

--- HTML/index.php
+++ HTML/index.php
@@ -670,7 +670,115 @@
 					</div>
 				</div>
 			</section>
-			
+
+			<section class="section section-no-border custom-background-color-1 m-none">
+				<div class="container">
+					<h2><strong>Colaboradores</strong></h2>
+		
+					<ul class="nav nav-pills sort-source custom-nav-sort mb-xlg" data-sort-id="team" data-option-key="filter">
+						<li data-option-value="*" class="active"><a href="#">Todos</a></li>
+						<!-- <li data-option-value=".leadership"><a href="#">Leadership</a></li>
+						<li data-option-value=".marketing"><a href="#">Marketing</a></li>
+						<li data-option-value=".development"><a href="#">Development</a></li>
+						<li data-option-value=".design"><a href="#">Design</a></li> -->
+					</ul>
+
+					<!-- <hr> -->
+
+					<div class="row">
+
+						<div class="sort-destination-loader sort-destination-loader-showing">
+							<ul class="team-list sort-destination" data-sort-id="team">
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item leadership">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="about-me.html">
+												<img src="img/team-1.jpg" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner">John Doe</span>
+													<span class="thumb-info-type">CEO</span>
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item marketing">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="about-me.html">
+												<img src="img/team-2.jpg" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner">Jessica Doe</span>
+													<span class="thumb-info-type">Marketing</span>
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item marketing">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="about-me.html">
+												<img src="img/team-2.jpg" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner">Jessica Doe</span>
+													<span class="thumb-info-type">Marketing</span>
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item marketing">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="about-me.html">
+												<img src="img/team-2.jpg" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner">Jessica Doe</span>
+													<span class="thumb-info-type">Marketing</span>
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>		
+			</section>
+
 			<!-- <section id="blog" class="section section-no-border background-color-light m-none">
 				<div class="container">
 					<div class="row">
@@ -761,7 +869,7 @@
 			<div id="say-hello" class="container-fluid">
 				<div class="row">
 					<div class="col-md-6 p-none">
-						<section class="section section-no-border match-height  m-none">
+						<section class="section section-no-border match-height background-color-primary m-none">
 							<div class="row m-none">
 								<div class="col-half-section col-half-section-right mr-md">
 									<h2 class="text-color-quaternary text-uppercase font-weight-extra-bold">Diga olá</h2>
